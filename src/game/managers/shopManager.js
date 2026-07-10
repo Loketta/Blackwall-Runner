047a55286ad542c17b1a12431de93f8aa123cdfb
Loketta <ameizingpotg@gmail.com>
2026-07-10 18:37:35 +0100

    Refactor: migrate writable collection managers

--- src/game/managers/shopManager.js
+++ src/game/managers/shopManager.js
@@ -1,37 +1,39 @@
-const fs = require("fs");
 const path = require("path");
+const {
+  createJsonCollectionRepository
+} = require("../repositories/jsonCollectionRepository");
 
-const shopsPath = path.join(__dirname, "../../../data/Shops/shops.json");
+const shopRepository = createJsonCollectionRepository({
+  filePath: path.join(
+    __dirname,
+    "../../../data/Shops/shops.json"
+  ),
+  indentation: 2
+});
 
 function loadShops() {
-    const shopData = fs.readFileSync(shopsPath, "utf8");
-    return JSON.parse(shopData);
+  return shopRepository.loadAll();
 }
 
 function saveShops(shops) {
-    const shopData = JSON.stringify(shops, null, 2);
-    fs.writeFileSync(shopsPath, shopData);
+  return shopRepository.saveAll(shops);
 }
 
 function loadShop(shopId) {
-    const shops = loadShops();
-
-    return shops.find(function(shop) {
-        return shop.id === shopId;
-    });
+  return shopRepository.loadById(shopId);
 }
 
 function loadShopsAtLocation(locationId) {
-    const shops = loadShops();
-
-    return shops.filter(function(shop) {
-        return shop.locationId === locationId;
+  return shopRepository
+    .loadAll()
+    .filter(function (shop) {
+      return shop.locationId === locationId;
     });
 }
 
 module.exports = {
-    loadShops,
-    saveShops,
-    loadShop,
-    loadShopsAtLocation
+  loadShops,
+  saveShops,
+  loadShop,
+  loadShopsAtLocation
 };
--- src/game/managers/worldObjectManager.js
+++ src/game/managers/worldObjectManager.js
@@ -1,44 +1,31 @@
-const fs = require("fs");
 const path = require("path");
+const {
+  createJsonCollectionRepository
+} = require("../repositories/jsonCollectionRepository");
 
-const worldObjectsPath = path.join(
-  __dirname,
-  "../../../data/worldObjects/worldObjects.json"
-);
+const worldObjectRepository =
+  createJsonCollectionRepository({
+    filePath: path.join(
+      __dirname,
+      "../../../data/worldObjects/worldObjects.json"
+    ),
+    indentation: 2
+  });
 
 function loadWorldObjects() {
-  const fileData = fs.readFileSync(worldObjectsPath, "utf8");
-  return JSON.parse(fileData);
+  return worldObjectRepository.loadAll();
 }
 
 function saveWorldObjects(worldObjects) {
-  const fileData = JSON.stringify(worldObjects, null, 2);
-  fs.writeFileSync(worldObjectsPath, fileData);
+  return worldObjectRepository.saveAll(worldObjects);
 }
 
 function loadWorldObject(objectId) {
-  const worldObjects = loadWorldObjects();
-
-  return worldObjects.find(function (worldObject) {
-    return worldObject.id === objectId;
-  });
+  return worldObjectRepository.loadById(objectId);
 }
 
 function saveWorldObject(updatedObject) {
-  const worldObjects = loadWorldObjects();
-
-  const objectIndex = worldObjects.findIndex(function (worldObject) {
-    return worldObject.id === updatedObject.id;
-  });
-
-  if (objectIndex === -1) {
-    return false;
-  }
-
-  worldObjects[objectIndex] = updatedObject;
-  saveWorldObjects(worldObjects);
-
-  return true;
+  return worldObjectRepository.update(updatedObject);
 }
 
 module.exports = {
